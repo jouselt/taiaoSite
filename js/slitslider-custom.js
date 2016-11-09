aeeec68df01a0c8bfd16c65e5779d62c1067e466
Jouselt Fernandez <jouselt@gmail.com>
2016-11-09 18:45:45 -0500

    fixes

--- js/slitslider-custom.js
+++ js/slitslider-custom.js
@@ -1,50 +1,50 @@
-$(function() {
-			
-				var Page = (function() {
+$(function () {
 
-					var $nav = $( '#nav-dots > span' ),
-						slitslider = $( '#slider' ).slitslider( {
-							onBeforeChange : function( slide, pos ) {
+    var Page = (function () {
 
-								$nav.removeClass( 'nav-dot-current' );
-								$nav.eq( pos ).addClass( 'nav-dot-current' );
+        var $nav = $('#nav-dots > span'),
+            slitslider = $('#slider').slitslider({
+                onBeforeChange: function (slide, pos) {
 
-							}
-						} ),
+                    $nav.removeClass('nav-dot-current');
+                    $nav.eq(pos).addClass('nav-dot-current');
 
-						init = function() {
+                }
+            }),
 
-							initEvents();
-							
-						},
-						initEvents = function() {
+            init = function () {
 
-							$nav.each( function( i ) {
-							
-								$( this ).on( 'click', function( event ) {
-									
-									var $dot = $( this );
-									
-									if( !slitslider.isActive() ) {
+                initEvents();
 
-										$nav.removeClass( 'nav-dot-current' );
-										$dot.addClass( 'nav-dot-current' );
-									
-									}
-									
-									slitslider.jump( i + 1 );
-									return false;
-								
-								} );
-								
-							} );
+            },
+            initEvents = function () {
 
-						};
+                $nav.each(function (i) {
 
-						return { init : init };
+                    $(this).on('click', function (event) {
 
-				})();
+                        var $dot = $(this);
 
-				Page.init();
+                        if (!slitslider.isActive()) {
 
-			}); 
+                            $nav.removeClass('nav-dot-current');
+                            $dot.addClass('nav-dot-current');
+
+                        }
+
+                        slitslider.jump(i + 1);
+                        return false;
+
+                    });
+
+                });
+
+            };
+
+        return {init: init};
+
+    })();
+
+    Page.init();
+
+});
